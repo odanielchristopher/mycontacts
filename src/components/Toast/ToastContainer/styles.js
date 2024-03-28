72961998a7ae4251e8033690046f1df60501ee59
Daniel Rodrigues <o.dani.br@gmail.com>
2024-03-28 13:01:07 -0300

    criando o ToastComponent

---
+++ src/components/Toast/ToastContainer/styles.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  bottom: 4.8rem;
+  left: 50%;
+  position: fixed;
+  transform: translateX(-50%);
+  z-index: 2;
+`;
