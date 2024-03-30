d34bd0b731118a8e9940a1730f80aa9e8adecb3e
Daniel Rodrigues <o.dani.br@gmail.com>
2024-03-29 22:54:40 -0300

    criando o ToastMessage

--- src/components/Toast/ToastContainer/index.js
+++ src/components/Toast/ToastContainer/index.js
@@ -1,9 +1,24 @@
+import { useState } from 'react';
 import { Container } from './styles';
 
+import ToastMessage from '../ToastMessage';
+
 export default function ToastContainer() {
+  const [messages] = useState([
+    { id: Math.random(), type: 'default', text: 'Default Text' },
+    { id: Math.random(), type: 'danger', text: 'Danger Text' },
+    { id: Math.random(), type: 'success', text: 'Success Text' },
+  ]);
+
   return (
     <Container>
-      <h1>ToastContainer</h1>
+      {messages.map((message) => (
+        <ToastMessage
+          key={message.id}
+          type={message.type}
+          text={message.text}
+        />
+      ))}
     </Container>
   );
 }
---
+++ src/components/Toast/ToastMessage/styles.js
@@ -0,0 +1,24 @@
+import styled, { css } from 'styled-components';
+
+const ContainerVariants = {
+  default: css`background: ${({ theme }) => theme.colors.primary.main};`,
+  success: css`background: ${({ theme }) => theme.colors.success.main};`,
+  danger: css`background: ${({ theme }) => theme.colors.danger.main};`,
+};
+
+export const Container = styled.div`
+  align-items: center;
+  border-radius: .4rem;
+  box-shadow: 0rem 2.0rem 2.0rem -1.6rem #00000040;
+  color: #FFF;
+  display: flex;
+  justify-content: center;
+  gap: .8rem;
+  padding: 1.6rem 3.2rem;
+
+  ${({ type }) => ContainerVariants[type] || ContainerVariants.default}
+
+  & + & {
+    margin-top: 1.2rem;
+  }
+`;
