7e1b1a93b74d60fb1023efe3a21457b50e1289f8
Daniel Rodrigues <o.dani.br@gmail.com>
2024-04-04 22:45:04 -0300

    fazendo a requisicao de update no EditContact

--- src/components/ContactForm/index.js
+++ src/components/ContactForm/index.js
@@ -40,6 +40,12 @@ const ContactForm = forwardRef(({ buttonLabel, onSubmit }, ref) => {
       setPhone(formatPhone(contact.phone ?? ''));
       setCategoryId(contact.category_id ?? '');
     },
+    resetFields: () => {
+      setName('');
+      setEmail('');
+      setPhone(formatPhone(''));
+      setCategoryId('');
+    },
   }), []);
 
   useEffect(() => {
@@ -89,10 +95,6 @@ const ContactForm = forwardRef(({ buttonLabel, onSubmit }, ref) => {
     });
 
     setIsSubmitting(false);
-    setName('');
-    setEmail('');
-    setPhone('');
-    setCategoryId('');
   }
 
   return (
--- src/pages/EditContact/index.js
+++ src/pages/EditContact/index.js
@@ -37,8 +37,28 @@ export default function EditContact() {
     loadContact();
   }, [id, history]);
 
-  function handleSubtmit() {
+  async function handleSubtmit(formData) {
+    try {
+      const contact = {
+        name: formData.name,
+        email: formData.email,
+        phone: formData.phone,
+        category_id: formData.categoryId,
+      };
 
+      const contactData = await ContactsService.updateContact(id, contact);
+
+      setContactName(contactData.name);
+      toast({
+        type: 'success',
+        text: 'Contato editado com sucesso!',
+      });
+    } catch {
+      toast({
+        type: 'danger',
+        text: 'Ocorreu um erro ao editar o contato!',
+      });
+    }
   }
 
   return (
